2b0e3b88d845d3ddd6edd01c9df44c67e7ea29fd
laa252159@gmail.com <laa252159@gmail.com@58c48679-0f66-266f-7e91-62d32139d9ac>
2015-02-27 15:13:33 +0000

    persons table first

--- sql/POSTGR_script.sql
+++ sql/POSTGR_script.sql
@@ -1,1 +1,17 @@
-dd
+-- Table: "PERSONS"
+
+-- DROP TABLE "PERSONS";
+
+CREATE TABLE "PERSONS"
+(
+  id bigserial NOT NULL,
+  login character varying(20)[] NOT NULL,
+  password character varying(20)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "PERSONS"
+  OWNER TO postgres;
+GRANT ALL ON TABLE "PERSONS" TO postgres;
+GRANT ALL ON TABLE "PERSONS" TO public;
