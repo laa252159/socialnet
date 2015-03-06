9e4a75f7edc69ef0ec36c9dee5f7b2e5397b717f
Aleksandr_Lobov <laa252159@gmail.com>
2015-03-06 12:42:19 -0300

    Ломаем все!!! переходим на XML конфиг

--- src/main/java/com/epam/socialnet/controller/HomeController.java
+++ src/main/java/com/epam/socialnet/controller/HomeController.java
@@ -1,8 +1,5 @@
 package com.epam.socialnet.controller;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
