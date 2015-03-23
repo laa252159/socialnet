5281b78e225948915b452b1f9918893d836d7cb3
Aleksandr_Lobov <laa252159@gmail.com>
2015-03-23 11:41:05 -0300

    Gallery back

--- src/main/java/com/epam/socialnet/dao/AlbumDaoImple.java
+++ src/main/java/com/epam/socialnet/dao/AlbumDaoImple.java
@@ -1,33 +1,64 @@
 package com.epam.socialnet.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.epam.socialnet.model.Album;
 
 public class AlbumDaoImple implements AlbumDao {
 
+	private JdbcTemplate jdbcTemplate;
+
+	public AlbumDaoImple(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
 	@Override
 	public void createAlbum(Album album) {
-		// TODO Auto-generated method stub
+		String sql = "INSERT INTO albums (person_id, name, description)"
+				+ " VALUES (?, ?, ?)";
+		jdbcTemplate.update(sql, album.getPersonId(), album.getName(),
+				album.getDescription());
 
 	}
 
 	@Override
 	public void updateAlbum(Album album) {
-		// TODO Auto-generated method stub
-
+		String sql = "UPDATE albums SET "
+				+ "name = ?, description = ? where id = ?";
+		jdbcTemplate.update(sql, album.getName(), album.getDescription());
 	}
 
 	@Override
 	public void deleteAlbum(Long albumId) {
-		// TODO Auto-generated method stub
+		String sql = "DELETE FROM albums WHERE id = ?";
+		jdbcTemplate.update(sql, albumId);
 
 	}
 
 	@Override
 	public List<Album> getAlbumsForPerson(Long personId) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM albums WHERE person_id = ?";
+		List<Album> listAlbum = jdbcTemplate.query(sql, new RowMapper<Album>() {
+
+			@Override
+			public Album mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Album album = new Album();
+				album.setId(rs.getLong("id"));
+				album.setPersonId(rs.getLong("person_id"));
+				album.setName(rs.getString("name"));
+				album.setDescription(rs.getString("description"));
+				return album;
+			}
+		}, personId);
+
+		return listAlbum;
 	}
 
 }
